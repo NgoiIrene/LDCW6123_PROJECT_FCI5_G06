483a2b5be695e4d1de13cd0daeddb90d9eef5e9b
DangHuiSin <danghuisin0262@gmail.com>
2025-09-24 13:30:34 +0800

    menu def, valid choice

--- MCD_kiosk.cpp
+++ MCD_kiosk.cpp
@@ -97,3 +97,107 @@ void Display_MenuC()
     cout << "        |_______|_____________________________________|_______________|" << endl;
     cout << endl;
 }
+
+void Display_MenuD()
+{
+    cout << "         ________________________________________________" << endl;
+    cout << "        |  No   |          Items          |  Price(RM)   |" << endl;
+    cout << "        |_______|_________________________|______________|" << endl;
+    cout << "        |   1   |Coca-Cola                |" << setw(4) << " " << " 5.65" << setw(4) << " " << " |" << endl;
+    cout << "        |   2   |Coca-Cola Zero Sugar     |" << setw(4) << " " << " 5.65" << setw(4) << " " << " |" << endl;
+    cout << "        |   3   |100 Plus                 |" << setw(4) << " " << " 5.65" << setw(4) << " " << " |" << endl;
+    cout << "        |   4   |Iced Lemon Tea           |" << setw(4) << " " << " 5.65" << setw(4) << " " << " |" << endl;
+    cout << "        |   5   |Iced Milo                |" << setw(4) << " " << " 6.04" << setw(4) << " " << " |" << endl;
+    cout << "        |   6   |Hot Milo                 |" << setw(4) << " " << " 6.04" << setw(4) << " " << " |" << endl;
+    cout << "        |_______|_________________________|______________|" << endl;
+    cout << endl;
+}
+
+void Display_MenuE()
+{
+    cout << "         ________________________________________________________________________" << endl;
+    cout << "        |  No   |                      Items                     |  Price (RM)   |" << endl;
+    cout << "        |_______|________________________________________________|_______________|" << endl;
+    cout << "        |   1   |Nasi Lemak McD + 2pcs Fried Chicken McD Regular |" << setw(4) << " " << " 16.79" << setw(4) << " " << " |" << endl;
+    cout << "        |   2   |Nasi Lemak McD + 2pcs Fried Chicken McD Spicy   |" << setw(4) << " " << " 16.79" << setw(4) << " " << " |" << endl;
+    cout << "        |   3   |Nasi Lemak McD                                  |" << setw(4) << " " << "  8.02" << setw(4) << " " << " |" << endl;
+    cout << "        |   4   |Medium Chicken Porridge McD                     |" << setw(4) << " " << "  8.68" << setw(4) << " " << " |" << endl;
+    cout << "        |   5   |Small Chicken Porridge McD                      |" << setw(4) << " " << "  7.26" << setw(4) << " " << " |" << endl;
+    cout << "        |   6   |Plain Nasi Lemak                                |" << setw(4) << " " << "  2.83" << setw(4) << " " << " |" << endl;
+    cout << "        |_______|________________________________________________|_______________|" << endl;
+    cout << endl;
+}
+
+void Display_MenuF()
+{
+    cout << "         _____________________________________________________" << endl;
+    cout << "        |  No   |            Items            |  Price (RM)   |" << endl;
+    cout << "        |_______|_____________________________|_______________|" << endl;
+    cout << "        |   1   |Apple Pie                    |" << setw(4) << " " << "  5.52" << setw(4) << " " << " |" << endl;
+    cout << "        |   2   |Oreo Mc Flurry               |" << setw(4) << " " << "  7.78" << setw(4) << " " << " |" << endl;
+    cout << "        |   3   |French Fries                 |" << setw(4) << " " << "  6.79" << setw(4) << " " << " |" << endl;
+    cout << "        |   4   |Chocolate Sundae             |" << setw(4) << " " << "  6.46" << setw(4) << " " << " |" << endl;
+    cout << "        |   5   |Strawberry Sundae            |" << setw(4) << " " << "  6.46" << setw(4) << " " << " |" << endl;
+    cout << "        |   6   |Corn                         |" << setw(4) << " " << "  5.85" << setw(4) << " " << " |" << endl;
+    cout << "        |_______|_____________________________|_______________|" << endl;
+    cout << endl;
+}
+
+int get_valid_choice()
+{
+    int choice;
+    char cont;
+
+    while (true)
+    {
+        cout << "Please choose one item by entering a number (1 to 6): ";
+        cin >> choice;
+
+        if (cin.fail() || choice < 1 || choice > 6)
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invalid input! Please enter a number between 1 and 6." << endl
+                 << endl;
+
+            while (true)
+            {
+                cout << "Do you want to continue order items? (y / n): ";
+                cin >> cont;
+                cont = tolower(cont);
+
+                if (cin.fail())
+                {
+                    cin.clear();
+                    cin.ignore(1000, '\n');
+                    cout << "Invalid input! Please enter y / n." << endl
+                         << endl;
+                    continue;
+                }
+
+                if (cont == 'n')
+                {
+                    cout << "Due to invalid input, the system is cancelling your order." << endl;
+                    cout << "Order cancelled.... Welcome back! Have a nice day!" << endl;
+                    exit(0);
+                }
+
+                else if (cont == 'y')
+                {
+                    break; // Go back to ask for choice again
+                }
+
+                else
+                {
+                    cout << "Invalid input! Please enter y / n." << endl
+                         << endl;
+                }
+            }
+        }
+        else
+        {
+            cin.ignore(1000, '\n');
+            return choice;
+        }
+    }
+}
